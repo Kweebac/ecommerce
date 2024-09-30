b1b7e33c4dc41e3628626a570f7009854bf3e997
Kweebac <06.amarshall@gmail.com>
2024-09-30 17:50:46 +0100

    Can now open links in separate tabs

--- client/src/components/Tables/Rows.tsx
+++ client/src/components/Tables/Rows.tsx
@@ -1,16 +1,7 @@
 import { flexRender } from "@tanstack/react-table";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Rows({ table }) {
-  const navigate = useNavigate();
-
-  function attemptNavigate(cell, id = cell.id.split("_")[1]) {
-    const cells = cell.row.getAllCells();
-    const itemId = cells[cells.length - 1].renderValue();
-
-    if (id === "url" || id === "name") navigate(itemId);
-  }
-
   return (
     <>
       {table.getRowModel().rows.map((row) => (
@@ -18,15 +9,28 @@ export default function Rows({ table }) {
           key={row.id}
           className="grid grid-flow-col items-center justify-start border-t border-t-gray-300"
         >
-          {row.getVisibleCells().map((cell) => (
-            <div
-              key={cell.id}
-              style={{ width: cell.column.getSize() }}
-              onClick={() => attemptNavigate(cell)}
-            >
-              {flexRender(cell.column.columnDef.cell, cell.getContext())}
-            </div>
-          ))}
+          {row.getVisibleCells().map((cell) => {
+            const cells = cell.row.getAllCells();
+            const url = cells[cells.length - 1].renderValue();
+
+            const id = cell.id.split("_")[1];
+            if (id === "name" || id === "url")
+              return (
+                <Link
+                  to={url}
+                  key={cell.id}
+                  style={{ width: cell.column.getSize() }}
+                >
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </Link>
+              );
+            else
+              return (
+                <div key={cell.id} style={{ width: cell.column.getSize() }}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </div>
+              );
+          })}
         </div>
       ))}
     </>
